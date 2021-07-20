ddfe4447a9c699a768f3b5f442ba6cc9b82fc1b1
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-07-20 18:57:03 +0300

    completed lesson 8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,52 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+export type StateType = PeopleType[]
+export type PeopleType = { _id: number; name: string; age: number }
+export type ActionType = { type: ActionsTypesType; payload: PayloadsType }
+
+export type ActionsTypesType = 'sort' | 'check'
+export type PayloadsType = 'up' | 'down' | number
+
+export const homeWorkReducer = (
+  state: StateType,
+  action: ActionType
+): StateType => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      if (action.payload === 'up') {
+        const newState = [...state]
+        return newState.sort((a: PeopleType, b: PeopleType) => {
+          const nameA = a.name.toLowerCase(),
+            nameB = b.name.toLowerCase()
+          if (nameA < nameB)
+            //сортируем строки по возрастанию
+            return -1
+          if (nameA > nameB) return 1
+          return 0 // Никакой сортировки
+        })
+      }
+      if (action.payload === 'down') {
+        const newState = [...state]
+        return newState.sort((a: PeopleType, b: PeopleType) => {
+          const nameA = a.name.toLowerCase(),
+            nameB = b.name.toLowerCase()
+          if (nameA > nameB)
+            //сортируем строки по убыванию
+            return -1
+          if (nameA < nameB) return 1
+          return 0 // Никакой сортировки
+        })
+      }
+      return state
     }
+    case 'check': {
+      // need to fix
+      return state.reduce(
+        (res: StateType, p: PeopleType) =>
+          p.age >= action.payload ? [...res, p] : [...res],
+        []
+      )
+    }
+    default:
+      return state
+  }
 }
