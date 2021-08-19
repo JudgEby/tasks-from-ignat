49186c8acc9d2272a95b3d0440403ccbe3684be4
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-08-19 14:36:22 +0300

    completed lesson 10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,38 +1,52 @@
 import React from 'react'
+import styles from './HW10.module.css'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppStoreType } from './bll/store'
+import { loadingAC } from './bll/loadingReducer'
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+  // useSelector, useDispatch
+  const loading = useSelector<AppStoreType, boolean>(
+    (state) => state.loading.isLoading
+  )
 
-    const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+  const dispatch = useDispatch()
 
-    return (
-        <div>
-            <hr/>
-            homeworks 10
+  const setLoading = () => {
+    dispatch(loadingAC({ isLoading: true }))
+    setTimeout(() => {
+      dispatch(loadingAC({ isLoading: false }))
+    }, 2000)
+  }
 
-            {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
-                    <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
-                    </div>
-                )
-            }
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
+  return (
+    <div>
+      <hr />
+      homeworks 10
+      {/*should work (должно работать)*/}
+      {loading ? (
+        <div className={styles.ldsRoller}>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
         </div>
-    )
+      ) : (
+        <div>
+          <SuperButton onClick={setLoading}>set loading...</SuperButton>
+        </div>
+      )}
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<Alternative/>*/}
+      <hr />
+    </div>
+  )
 }
 
 export default HW10
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,35 @@
+type InitStateType = typeof initState
+
+type ActionsTypes = LoadingACType
+
+enum IS_LOADING {
+  IS_LOADING = 'IS_LOADING',
+}
+
 const initState = {
-
+  isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        case '': {
-            return state
-        }
-        default: return state
+export const loadingReducer = (
+  state = initState,
+  action: ActionsTypes
+): InitStateType => {
+  // fix any
+  switch (action.type) {
+    case IS_LOADING.IS_LOADING: {
+      return { ...state, ...action.payload }
     }
+    default:
+      return state
+  }
 }
 
-export const loadingAC = (): any => {} // fix any
+type LoadingACType = {
+  type: typeof IS_LOADING.IS_LOADING
+  payload: { isLoading: boolean }
+}
+
+export const loadingAC = (payload: { isLoading: boolean }): LoadingACType => ({
+  type: IS_LOADING.IS_LOADING,
+  payload,
+}) // fix any
